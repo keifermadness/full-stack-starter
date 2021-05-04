33c94cfdba4a97e6040e49f3955b4c5278e1b049
Keifer Gilbert <keifergilbert@gmail.com>
2021-05-04 16:45:01 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+fullName = (first, last) => {
+    return `${first} ${last}`;
+}
+
+alert(fullName('Kristine', 'Hudgens'));
+
+
+
+const user = {
+    name: 'Kristine',
+    email: 'kristine@devcamp.com',
+    favorites: [
+        'Chipotle'
+        'Chik fil A'
+    ]
+}
+
+
+user.favorites 
+
+
